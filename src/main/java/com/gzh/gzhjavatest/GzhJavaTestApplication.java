8d757e0e4a3ca242ba3cca34351f698e646393dd
MaverickGao <zhiheng222@126.com>
2023-05-07 20:56:21 +0800

    first commit

---
+++ src/main/java/com/gzh/gzhjavatest/GzhJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.gzh.gzhjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzhJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GzhJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzh/gzhjavatest/GzhJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzh.gzhjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzhJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
